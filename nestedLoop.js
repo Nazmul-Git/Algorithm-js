86a8e30e3f9c3efe634f382904ca72e438ac76cd
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-09 17:06:57 +0600

    Loops are increase time complexity.

---
+++ nestedLoop.js
@@ -0,0 +1,26 @@
+
+let n=5;
+// for(let i=0; i<n; i++){  
+//     console.log('*');  // 5 times
+// }
+// n^1
+
+
+// for(let i=0; i<n; i++){  
+//     for(let j=0; j<n; j++){
+//         console.log('*');  // 25 times
+//     }
+// }
+// n^2
+
+
+for(let i=0; i<n; i++){  
+    for(let j=0; j<n; j++){
+        for(let k=0; k<n; k++){
+            console.log('*');  // 125 times
+        }
+    }
+}
+//  n^3
+
+// So, Loops are increase time complexity.
