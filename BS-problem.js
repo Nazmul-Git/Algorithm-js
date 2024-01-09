f51aa9d4fc849f116bcbf73e7f0958ca98b124b4
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-09 22:45:03 +0600

    Implement a function that decide a number is plaindrome or not.

--- BS-problem.js
+++ BS-problem.js
@@ -3,3 +3,9 @@ for(let i=0, j=arr.length-1; i<=j; i++,j--){
     if(arr[i]===arr[j]) console.log('palindrome')
     else console.log('not palindrome')
 }
+
+//  Number is palindrome or not
+const palindrome=(x)=>{
+    return x==x.toString().split('').reverse().join('');
+}
+console.log(palindrome(121))
