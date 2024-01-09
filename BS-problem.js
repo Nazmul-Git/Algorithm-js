4ef513d2541e6fc8eac40de1da5a1a4b9fb3e9d2
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-09 23:19:13 +0600

    Implement a function that decide an array elements are plaindrome or not.

--- BS-problem.js
+++ BS-problem.js
@@ -1,8 +1,20 @@
-let arr=[1,2,3,4,5,7,3,2,1]
+
+const isPalindrome=(arr)=>{
 for(let i=0, j=arr.length-1; i<=j; i++,j--){
-    if(arr[i]===arr[j]) console.log('palindrome')
-    else console.log('not palindrome')
+    if(arr[i]===arr[j]){
+        i++;
+        j--;
+    }
+    else return 'not palindrome';
 }
+return 'is palindrome';
+};
+console.log(isPalindrome([1,2,3,4,5,7,3,2,1]))
+
+
+
+///////////////////////////////////////////////////////////////
+
 
 //  Number is palindrome or not
 const palindrome=(x)=>{
