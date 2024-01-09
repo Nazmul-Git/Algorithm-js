cdb9d9b12588a5dcd9ea8781a1c169d8c0d11bd6
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-09 11:55:41 +0600

    Problem-1539 solve.

--- BS-problem-4.js
+++ BS-problem-4.js
@@ -19,7 +19,6 @@ Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
 
 const findMedianSortedArrays = (nums1, nums2) => {
   let marge = nums1.concat(nums2).sort((a, b) => a - b);
-  let l = 0;
   let n = marge.length;
   console.log(n);
   if (n % 2 === 0) {
