959ad78d5cad4555808a4f7629ebde46078efbff
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-09 18:54:32 +0600

    Space complexity and count.

--- nestedLoop.js
+++ nestedLoop.js
@@ -1,5 +1,5 @@
 
-let n=5;
+// let n=5;
 // for(let i=0; i<n; i++){  
 //     console.log('*');  // 5 times
 // }
@@ -13,14 +13,23 @@ let n=5;
 // }
 // n^2
 
-
-for(let i=0; i<n; i++){  
-    for(let j=0; j<n; j++){
-        for(let k=0; k<n; k++){
-            console.log('*');  // 125 times
-        }
-    }
-}
+// let count=0;
+// for(let i=0; i<n; i++){  
+//     for(let j=0; j<n; j++){
+//         for(let k=0; k<n; k++){
+//             count++;  
+//         }
+//     }
+// }
+// console.log(count); // 125 times
 //  n^3
 
 // So, Loops are increase time complexity.
+
+
+
+let n=5, count=0;
+for(let i=0; i<100*n; i++){
+    count++;  
+}
+console.log(count); // 500 times but here time complexity is 5. Time complexity always depends on user-input/n/loop 
