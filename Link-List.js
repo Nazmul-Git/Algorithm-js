b65019f3ce98e2a8b40c1ce6d905dcf069a5aa72
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-14 00:46:46 +0600

    Add append and prepend method(not build in) for creating linklist that appen add node in last and prepend add in first.

--- Link-List.js
+++ Link-List.js
@@ -22,9 +22,9 @@
 ////////////////////////////////////////////
 
 class Node {
-    constructor(value){
+    constructor(value,next=null){
         this.value=value;
-        this.next=null
+        this.next=next;
     }
 }
 
@@ -33,29 +33,28 @@ class LinkList {
     tail;
     length;
     constructor(value){
-        // let node={
-        //     value:value,
-        //     next:null
-        // }
-        let node=new Node(value,null)
+
+        let node=new Node(value)
         this.head=node;
         this.tail=node;
         this.length=1;
         // console.log('This : ',this);
     }
     append(value){ 
-        let node={
-            value:value, 
-            next:null
-        }
+        let node=new Node(value)
         this.tail.next=node;
         this.tail=node;
         this.length++;
     }
+    prepend(value){
+        let node = new Node(value,this.head);
+        this.head=node;
+    }
 
     print(){
         let data=this.head;
-        while(data.next != null){
+        // console.log(data);
+        while(data){
             console.log(data.value);
             data=data.next;
         }
@@ -67,4 +66,7 @@ list.append(200);
 list.append(300);
 list.append(400);
 list.append(500);
+list.append(600);
+list.prepend(10);
+list.append(700)
 list.print();
