103f8c18c769f6465d320427c3db00942d3bf5c1
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-19 08:38:32 +0600

    Implement link list.

---
+++ Link-list-CRUD.js
@@ -0,0 +1,60 @@
+class Node {
+    constructor(value, next=null){
+        this.value=value;
+        this.next=next;
+    }
+}
+
+class LinkList {
+    head;
+    tail;
+    length;
+    constructor(value){
+        let node= new Node(value);
+        this.head=node;
+        this.tail=node;
+        this.length=1;
+    }
+    append(value){
+        let node = new Node(value);
+        this.tail.next=node;
+        this.tail=node;
+    }
+    prepend(value){
+        let node= new Node(value, this.head);
+        this.head=node;
+    }
+    #getPreviousNode(index){
+        index--;
+        let data= this.head;
+        while(index){
+            data=data.next;
+            index--
+        }
+        return data;
+    }
+    insertAtAnyPosition(value, index){
+        if(index===0) this.prepend(value);
+        else if(index=== this.length) this.append(value);
+        else{
+            let previousNode=this.#getPreviousNode(index);
+            let newNode=new Node(value,previousNode.next);
+            previousNode.next=newNode;
+        }
+    }
+    print(){
+        let data = this.head;
+        while(data){
+            console.log(data.value);
+            data=data.next;
+        }
+    }
+
+    
+}
+
+let nodeList=new LinkList(10);
+nodeList.append(20);
+nodeList.prepend(5);
+nodeList.insertAtAnyPosition(15,2)
+nodeList.print()
