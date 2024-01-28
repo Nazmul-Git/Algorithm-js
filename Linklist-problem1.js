37666fafa648a4f970649bca89f65e3d8945be43
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-29 02:51:23 +0600

    Implement a method that delete a value at any index in linklist.

--- Linklist-problem1.js
+++ Linklist-problem1.js
@@ -1,67 +1,86 @@
 // var mergeTwoLists = function(list1, list2) {
-    
+
 // };
 // console.log(mergeTwoLists([1,3,4],[1,4,5]));
 
 class Node {
-    constructor(value, next=null){
-        this.value=value;
-        this.next=next;
-    }
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
-class LinkList{
-    head;
-    tail;
-    length;
-    constructor(value){
-        let node= new Node(value);
-        this.head=node;
-        this.tail=node;
-        this.length=1;
+class LinkList {
+  head;
+  tail;
+  length;
+  constructor(value) {
+    let node = new Node(value);
+    this.head = node;
+    this.tail = node;
+    this.length = 1;
+  }
+  append(value) {
+    let node = new Node(value);
+    this.tail.next = node;
+    this.tail = node;
+    // console.log(node);
+  }
+  appendMultiple(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      this.append(arr[i]);
     }
-    append(value){
-        let node=new Node(value);
-        this.tail.next=node;
-        this.tail=node;
-        // console.log(node);
+  }
+  prepend(value) {
+    let node = new Node(value, this.head);
+    this.head = node;
+  }
+  #getPreviousIndex(index) {
+    //4
+    index--; //3
+    let data = this.head; //head node
+    while (index) {
+      //3->2->1
+      data = data.next; // head node  = node er next
+      index--; //2->1
     }
-    appendMultiple(arr){
-        for(let i=1; i<arr.length; i++){
-            this.append(arr[i]);
-        }
+    return data;
+  }
+  insertAtPosition(value, index) {
+    if (index === 0) this.prepend(value);
+    else if (index === this.length) this.append(value);
+    else {
+      let previousNode = this.#getPreviousIndex(index);
+      let node = new Node(value, previousNode.next);
+      previousNode.next = node;
     }
-    prepend(value){
-        let node=new Node(value, this.head);
-        this.head=node;
+  }
+  #getCurrentIndex(index){
+    let data=this.head;
+    while(index){
+        data=data.next;
+        index--;
     }
-    #getPreviousIndex(index){ //4
-        index--; //3
-        let data=this.head; //head node
-        while(index){  //3->2->1
-            data=data.next;  // head node  = node er next 
-            index--; //2->1
-        }
-        return data;
+    return data;
+  }
+  deleteAtPosition(index) {
+    let previousNode=this.#getPreviousIndex(index);
+    previousNode.next=previousNode.next.next;
+
+  }
+  print() {
+    let data = this.head;
+    console.log(data);
+    while (data) {
+      console.log(data.value);
+      data = data.next;
     }
-    insertAtPosition(value, index){
-        let previousNode=this.#getPreviousIndex(index);
-        let node= new Node(value,previousNode.next);
-        previousNode.next=node;
-    }
-    print(){
-        let data=this.head;
-        console.log(data);
-        while(data){
-            console.log(data.value)
-            data=data.next;
-        }
-        
-    }
+  }
 }
-let nodeList=new LinkList(10);
+let nodeList = new LinkList(10);
 nodeList.append(20);
-nodeList.appendMultiple([30,40,50]);
-nodeList.prepend(1)
-nodeList.insertAtPosition(45,4);
-nodeList.print()
+nodeList.appendMultiple([30, 40, 50]);
+nodeList.prepend(1);
+nodeList.insertAtPosition(45, 4);
+nodeList.deleteAtPosition(4)
+nodeList.print();
