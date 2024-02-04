5bc303959b3819897f5f68536b07b542f72cff48
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-04 11:08:47 +0600

    Implement append mothod on doubly linklist.

--- Doubly_linklist.js
+++ Doubly_linklist.js
@@ -9,13 +9,23 @@ class Node{
 class Doubly_Linklist{
     head;
     tail;
-    length;
+    length=0;
     constructor(value){
         let node= new Node(value);
         this.head=node;
         this.tail=node;
         this.length++;
     }
+    append(value){
+        let node= new Node(value);
+        // console.log(node);
+        this.tail.next=node;
+        node.previous=this.tail
+        this.tail=node;
+        this.length++;
+         
+    }
 }
 let nodeList=new Doubly_Linklist(10);
+nodeList.append(20);
 console.log(nodeList);
