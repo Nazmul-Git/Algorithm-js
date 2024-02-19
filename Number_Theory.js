e1cc8b3e5b81a0986e7b50ab0cb2725cf02ce3d2
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-20 00:59:40 +0600

    IsPrime ? O(n) complexity.

---
+++ Number_Theory.js
@@ -0,0 +1,15 @@
+// isPrime
+///////////// O(n) /////////////
+// const isPrime=(n)=>{
+//     let count=0;
+//     for(let i=1;i<n; i++){
+//         if(n%2===0){
+//             count++
+//         }
+//     }
+//     if(count >=2 ) return false;
+//     else return true;
+// };
+// console.log(isPrime(11))
+
+//////////////
