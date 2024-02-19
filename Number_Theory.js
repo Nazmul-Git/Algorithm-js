879c6835a5a0dfe10862f23bb6a50a12717df9f6
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-20 01:35:57 +0600

    IsPrime ? best optimized of O(sqrt(n)) complexity.

--- Number_Theory.js
+++ Number_Theory.js
@@ -37,3 +37,16 @@
 
 
 //////////////////////  O(sqrt(n))  /////////////////////
+const isPrime=(n)=>{
+    let count=0;
+    for(let i=1;i< Math.sqrt(n); i++){
+        if(n%2===0){
+            count++
+        }
+    }
+    if(count > 1 ) return false;
+    else return true;
+};
+console.log(isPrime(11))
+console.log(isPrime(100))
+//  loop will be  execute 10 times cz, squire root of 100 is 10
