e9d63a6c5d4da1aa472230b782ddadeaf34b5a7a
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-25 02:42:57 +0600

    Implement binary to decimal conversion and time complexity is O(n).

--- Number-Binary_Decimal.js
+++ Number-Binary_Decimal.js
@@ -3,15 +3,13 @@ const binaryToDecimal = (binary) => {
   let arr = text.split("");
   let sum=0;
   let pow=arr.length-1;
-//   console.log(arr,pow);
+
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "1") {
       arr[i] = parseInt(arr[i]) * Math.pow(2, pow);
+      sum=sum+parseInt(arr[i]);
     }
     pow--;
-  }
-  for(let j=0; j<arr.length; j++){
-    sum=sum+parseInt(arr[j]);
   }
   return sum;
 };
