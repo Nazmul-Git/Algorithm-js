ef80e4b61119f72f1d81365a81622250395cfccb
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-24 12:18:40 +0600

    Implement how to find HCF of many numbers.

--- Number-HCF.js
+++ Number-HCF.js
@@ -1,10 +1,11 @@
 const HCF=(a,b)=>{
     let arr=[];
-    for(let i=1; i<=20; i++){
+    let minNum= a>b ? b : a;
+    for(let i=1; i<=minNum; i++){
         if(a%i===0 && b%i===0){
             arr.push(i);
         }
     }
     return arr.pop();
 }
-console.log(HCF(20,50));
+console.log(HCF(4,6));
