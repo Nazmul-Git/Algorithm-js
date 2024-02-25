9aafc3ecc7e5b7b24ec55177a371a2fd5245be9c
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-25 15:27:16 +0600

    Implement square border star pattern.

--- Star_pattern.js
+++ Star_pattern.js
@@ -1,10 +1,34 @@
 
 // Star square
-let str='';
-for(let i=0; i<=5;i++){
-    for(let j=0; j<=5; j++){
-        str+='*';
+
+// let str='';
+// for(let i=0; i<=5;i++){
+//     for(let j=0; j<=5; j++){
+//         str+='*';
+//     }
+//     console.log(str);
+//     str='';
+// }
+
+//////////////////////////////////////////////////
+
+// Star Square Border
+
+let star='';
+let n=5;
+for(let i=1; i<=n; i++){
+    for(let j=1; j<=n; j++){
+        if(i===1 || i===n){
+            star+='*';
+        }
+        else if(j===1 || j===n){
+            star+='*';
+        }
+        else{
+            star+=' ';
+        }
+        
     }
-    console.log(str);
-    str='';
-}
+    console.log(star);
+    star='';
+} 
