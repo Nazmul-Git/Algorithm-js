6a2c6314bd3db5a41360cd71c0b7783afca96e7e
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-25 21:20:54 +0600

    Implement cross star pattern.

--- Star_pattern.js
+++ Star_pattern.js
@@ -1,5 +1,5 @@
 
-// Star square
+// Star square  O(n^3)
 
 // let str='';
 // for(let i=0; i<=5;i++){
@@ -14,21 +14,34 @@
 
 // Star Square Border
 
-let star='';
-let n=5;
+// let star='';
+// let n=5;
+// for(let i=1; i<=n; i++){
+//     for(let j=1; j<=n; j++){
+//         if(i===1 || i===n || j===1 || j===n){
+//             star+='*';
+//         }
+//         else{
+//             star+=' ';
+//         }
+        
+//     }
+//     console.log(star);
+//     star='';
+// } 
+
+
+let cross='';
+let n=4;
 for(let i=1; i<=n; i++){
     for(let j=1; j<=n; j++){
-        if(i===1 || i===n){
-            star+='*';
-        }
-        else if(j===1 || j===n){
-            star+='*';
+        if(i==j || i+j===n+1){
+            cross+='*';
         }
         else{
-            star+=' ';
+            cross+=' ';
         }
-        
     }
-    console.log(star);
-    star='';
-} 
+    console.log(cross);
+    cross='';
+}
