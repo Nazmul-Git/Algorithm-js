39496679e39b0da8480af3e92baeec4020d3f3dd
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-02-29 02:33:49 +0600

    Implement diamond  pattern easy way part by part.

--- Star_pattern.js
+++ Star_pattern.js
@@ -110,34 +110,68 @@
 //     triangle='';
 // }
 
-///////////////////////////////
-let diamond = "";
-let n = 5;
-for (let i = n; i >= 1; i--) {
-  for (let s = 1; s < i; s++) {
-    diamond += " ";
-  }
-  for (let j = 1; j <= n - i + 1; j++) {
-    diamond += "*";
-  }
-  for (let k = 2; k <= n - i + 1; k++) {
-    diamond += "*";
-  }
+///////////////diamond////////////////
+// let diamond = "";
+// let n = 5;
+// for (let i = n; i >= 1; i--) {
+//   for (let s = 1; s < i; s++) {
+//     diamond += " ";
+//   }
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     diamond += "*";
+//   }
+//   for (let k = 2; k <= n - i + 1; k++) {
+//     diamond += "*";
+//   }
   
 
-  console.log(diamond);
-  diamond = "";
-}
-for(let i=2; i<=n; i++){
+//   console.log(diamond);
+//   diamond = "";
+// }
+// for(let i=2; i<=n; i++){
+//     for(let s=1; s<i; s++){
+//         diamond+=' '
+//     }
+//     for(let j=1; j<=n-i+1; j++){
+//         diamond+='*'
+//     }
+//     for (let k = 2; k <= n - i + 1; k++) {
+//         diamond += "*";
+//       }
+//     console.log(diamond);
+//     diamond='';
+// }
+
+
+/////////////// Diamond Easy Way ///////////////////
+let diamond = "";
+let n = 5;
+
+for(let i=1; i<=n; i++){
+    for(let s=1; s<=n-i; s++){
+        diamond+=' ';
+    }
+    for(let s=1; s<=i; s++){
+        diamond+='$'
+    }
     for(let s=1; s<i; s++){
-        diamond+=' '
+        diamond+='@'
     }
-    for(let j=1; j<=n-i+1; j++){
-        diamond+='*'
+    console.log(diamond);
+    diamond='';
+}
+
+
+for(let i=1; i<=n-1; i++){
+    for(let s=1; s<=i; s++){
+        diamond+=' ';
     }
-    for (let k = 2; k <= n - i + 1; k++) {
-        diamond += "*";
-      }
+    for(let s=1; s<=n-i; s++){
+        diamond+='$'
+    }
+    for(let s=1; s<n-i; s++){
+        diamond+='@'
+    }
     console.log(diamond);
     diamond='';
 }
