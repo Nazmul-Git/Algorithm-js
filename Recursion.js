4cdbc1b0a0de3d98f35a9c762f8035c76eed09aa
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-03 01:27:42 +0600

    Implement recursion in function right way.

--- Recursion.js
+++ Recursion.js
@@ -1,6 +1,17 @@
+// let sum=0;
+// const recursion=(n)=>{
+//     if(n==0) return;
+//     sum+=n;
+//     recursion(n-1);
+// };
+// let result=recursion(10);
+// console.log(result); //undefined 
+// console.log(sum); //55
+
+// solve
 const recursion=(n)=>{
-    if(n==0) return;
-    console.log(n);
-    recursion(n-1);
+    if(n==0) return n;
+    return n + recursion(n-1);
 };
-recursion(10);
+let result=recursion(10);
+console.log(result); //55
