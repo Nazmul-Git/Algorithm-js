f7a8971a0fad504c9ca223f35fbff76a6d0b6b72
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-06 16:32:40 +0600

    Traverse an array by using function recursion.

--- Recursion_sum_of_digits.js
+++ Recursion_sum_of_digits.js
@@ -1,7 +1,18 @@
-
+// Sum of Digits
 const sumOfDigits=(n)=>{
     if(n<10) return n;
     let newDigit=Math.floor(n/10);
     return (n%10)+ sumOfDigits(newDigit);
 }
-console.log(sumOfDigits(1279));
+// console.log(sumOfDigits(1279));
+
+
+// traverse an array / function  er loop
+
+const traverse=(arr, i)=>{
+    if(i===arr.length) return;
+    console.log(arr[i]);
+    traverse(arr, i+1);
+    return arr;
+}
+console.log(traverse([5,10,15,20,25,30], 0))
