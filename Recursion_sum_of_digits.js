112e64c90d025c5d36a6aeffbe756687eb38a60f
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-07 02:41:44 +0600

    Implement check palindrome method using recursion.

--- Recursion_sum_of_digits.js
+++ Recursion_sum_of_digits.js
@@ -28,13 +28,26 @@ console.log(sum([5,10,15,20,25,30], 0))
 
 
 // array reverse
-const reverseArr=(arr,start,end)=>{
-    if(start>end) return;
-    let temp=arr[start];
-    arr[start]=arr[end];
-    arr[end]=temp;
-    reverseArr(arr, start+1, end-1);
-    return arr;
+// const reverseArr=(arr,start,end)=>{
+//     if(start>end) return;
+//     let temp=arr[start];
+//     arr[start]=arr[end];
+//     arr[end]=temp;
+//     reverseArr(arr, start+1, end-1);
+//     return arr;
+// };
+// let arr=[10,20,30,40,50,60,70,80,90,100];
+// console.log(reverseArr(arr,0,arr.length-1))
+
+// palindrome 
+const palindrome=(arr,start,end)=>{
+    if(start>end) return true;
+    else if(arr[start]!==arr[end]) return false;
+    else{
+        return palindrome(arr, start+1, end-1);
+        
+    }
+    
 };
-let arr=[10,20,30,40,50,60,70,80,90,100];
-console.log(reverseArr(arr,0,arr.length-1))
+let arr=['a','b','c','b','a'];
+console.log(palindrome(arr,0,arr.length-1))
