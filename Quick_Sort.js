210b9c7575ddc4595c760b74ec0d7192b90cb28c
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-11 03:53:18 +0600

    Solve the Quick sort error.

--- Quick_Sort.js
+++ Quick_Sort.js
@@ -1,4 +1,32 @@
 const process = (arr, l, r) => {
+  let pivot = arr[r];
+  let i = l-1;
+  for (let j = l; j < r; j++) {
+    if (arr[j] <= pivot){
+        i++;
+        [arr[i],arr[j]]=[arr[j],arr[i]];
+    }
+  }
+
+  [arr[i+1],arr[r]]=[arr[r],arr[i+1]];
+  return i+1;
+};
+
+const quickSort = (arr, l, r) => {
+  if (l >= r) return;
+  let index = process(arr, l, r);
+  console.log(index);
+  quickSort(arr, l, index - 1);
+  quickSort(arr, index + 1, r);
+};
+let arr = [4, 9, 1, 0, 7];
+quickSort(arr, (l = 0), (r = arr.length - 1));
+console.log(arr);
+
+////////////////////////////////////////////////////////////
+
+/**
+ const process = (arr, l, r) => {
     let first = arr[l];
     let count = l;
     for (let i = l + 1; i <= r; i++) {
@@ -17,14 +45,4 @@ const process = (arr, l, r) => {
 
     return count;
 }
-
-const quickSort = (arr, l, r) => {
-    if (l >= r) return;
-    let index = process(arr, l, r);
-    quickSort(arr, l, index - 1);
-    quickSort(arr, index + 1, r);
-}
-
-let arr = [4, 9, 1, 0, 7];
-quickSort(arr, 0, arr.length - 1);
-console.log(arr); //[ 0, 1, 4, 7, 9 ]
+ */
