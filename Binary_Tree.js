f0eaed7d8992913a1540ae4778ff9f7a7cd3deb1
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-16 19:40:06 +0600

    Implement insert method on binary tree.

--- Binary_Tree.js
+++ Binary_Tree.js
@@ -12,7 +12,30 @@ class BinaryTree{
     constructor(value){
         this.root= new Node(value);
     }
+
+    insert(value){
+        let root=this.root;
+        while(true){
+            if(value > root.value){
+                //right 
+                if(root.right===null){
+                    root.right= new Node(value);
+                    break;
+                }
+                root=root.right;
+            }else{
+                // left
+                if(root.left===null){
+                    root.left= new Node(value);
+                    break;
+                }
+                root=root.left;
+            }
+        }
+    }
 }
 
 let tree= new BinaryTree(10);
+tree.insert(20);
+tree.insert(5);
 console.log(tree);
