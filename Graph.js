4a442a811857c7ffe609d11896206ce206f44aec
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-20 04:00:07 +0600

    Implement a directed and undirected graph by using hash map.

--- Graph.js
+++ Graph.js
@@ -3,31 +3,42 @@ class Graph {
     this.graph = {};
   }
 
-  addEdge(a, b, direct = false) {
+  addEdge(a, b, undirected) {
     if (this.graph[a]) {
       this.graph[a].push(b);
     } else {
       this.graph[a] = [b];
     }
 
-    if (!direct) {
-      if (this.graph[b]) {
-        this.graph[b].push(a);
-    }
-    else{
-        this.graph[b] = [a];
-    }
+    // console.log(undirected);
+    if (undirected) {
+    //   console.log(undirected);
+        if (this.graph[b]) {
+            this.graph[b].push(a);
+        } else {
+            this.graph[b] = [a];
+        }
+    }else{
+        this.graph[b]=[];
     }
   }
 }
 let graph = new Graph();
-graph.addEdge(1, 2);
-graph.addEdge(1, 3);
-graph.addEdge(1, 5);
-graph.addEdge(2, 3);
-graph.addEdge(2, 4);
-graph.addEdge(3, 4);
-graph.addEdge(3, 5);
-graph.addEdge(4, 6);
-graph.addEdge(5, 6);
+// undirected
+// graph.addEdge(1, 2, true);
+// graph.addEdge(1, 3, true);
+// graph.addEdge(1, 5, true);
+// graph.addEdge(2, 3, true);
+// graph.addEdge(2, 4, true);
+// graph.addEdge(3, 4, true);
+// graph.addEdge(3, 5, true);
+// graph.addEdge(4, 6, true);
+// graph.addEdge(5, 6, true);
+// graph.addEdge(7, 6, false);
+
+// directed
+graph.addEdge(1,2, false);
+graph.addEdge(4,2, false);
+graph.addEdge(4,3, false);
+graph.addEdge(3,2, false);
 console.log(graph);
