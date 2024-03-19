bb12e44d6fda630ba3b617e09f0e2abaa20fa239
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-19 22:26:13 +0600

    Implement a undirect graph right way by using hash map.

--- Graph.js
+++ Graph.js
@@ -1,36 +1,33 @@
+class Graph {
+  constructor() {
+    this.graph = {};
+  }
 
-
-class Graph{
-    constructor(){
-        this.graph={};
+  addEdge(a, b, direct = false) {
+    if (this.graph[a]) {
+      this.graph[a].push(b);
+    } else {
+      this.graph[a] = [b];
     }
 
-    addEdge(a,b){
-        if(this.graph[a]){
-            this.graph[a].push(b);
-        }
-        else{
-            this.graph[a]=[b];
-        }
+    if (!direct) {
+      if (this.graph[b]) {
+        this.graph[b].push(a);
     }
+    else{
+        this.graph[b] = [a];
+    }
+    }
+  }
 }
-let graph= new Graph();
-graph.addEdge(1,2);
-graph.addEdge(2,1);
-graph.addEdge(1,3);
-graph.addEdge(3,1);
-graph.addEdge(1,5);
-graph.addEdge(5,1);
-graph.addEdge(3,5);
-graph.addEdge(5,3);
-graph.addEdge(5,6);
-graph.addEdge(6,5);
-graph.addEdge(3,4);
-graph.addEdge(4,3);
-graph.addEdge(4,2);
-graph.addEdge(2,4);
-graph.addEdge(2,3);
-graph.addEdge(6,4);
-graph.addEdge(4,6);
-graph.addEdge(3,2);
+let graph = new Graph();
+graph.addEdge(1, 2);
+graph.addEdge(1, 3);
+graph.addEdge(1, 5);
+graph.addEdge(2, 3);
+graph.addEdge(2, 4);
+graph.addEdge(3, 4);
+graph.addEdge(3, 5);
+graph.addEdge(4, 6);
+graph.addEdge(5, 6);
 console.log(graph);
