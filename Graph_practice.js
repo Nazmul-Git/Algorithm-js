8443403dc639321782d1171b0ca0acd394990ad6
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-03-20 17:08:23 +0600

    Add addEdge method that, check there is already key name of x value in graph if there then push y value on this array else create new array with y value .

--- Graph_practice.js
+++ Graph_practice.js
@@ -4,7 +4,10 @@ class Graph{
     constructor(){
         this.graph={};
     }
-    
+    addEdge(x,y,dir=false){
+        if(this.graph[x]) this.graph[x].push(y);
+        else this.graph[x]=[y];
+    }
 }
 let graph= new Graph();
 graph.addEdge(1,2);
